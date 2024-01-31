4862fd6485ddf68311060ec10af73e9034243415
dilshad360 <dilshad360.dev@gmail.com>
2024-02-01 00:20:51 +0530

    feat: hero section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import Logo from '../assets/logo.svg'
 function NavBar() {
     return (
         <div className="absolute w-full text-white px-10 flex items-center justify-between">
-            <img src={Logo} className='w-36' alt="logo" />
+            <img src={Logo} className='w-40' alt="logo" />
             <div className='flex gap-10 text-xl'>
                 <a href="#">Home</a>
                 <a href="#">Score Board</a>
