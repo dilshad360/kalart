d5a64e67aaa820045bb1cc67a02164d02cae5de2
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-02-04 05:02:43 +0530

    updated: auto play

--- src/components/Result/Carousal.jsx
+++ src/components/Result/Carousal.jsx
@@ -59,10 +59,10 @@ const Carousal = () => {
         grabCursor={true}
         centeredSlides={true}
         loop={true}
-        // autoplay={{
-        //   delay: 2500,
-        //   disableOnInteraction: false,
-        // }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
         slidesPerView={'auto'}
         coverflowEffect={{
           rotate: 50,
